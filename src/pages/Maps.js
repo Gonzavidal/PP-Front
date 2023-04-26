34c9549f07310d50cf1e4f556805fb41f9bcfb5b
Gonzavidal <115666706+Gonzavidal@users.noreply.github.com>
2023-04-26 19:22:14 +0000

    Maps y marcador

--- src/pages/Maps.js
+++ src/pages/Maps.js
@@ -26,6 +26,7 @@ function Maps() {
   const onLoad = React.useCallback(function callback(map) {
     // This is just an example of getting and using the map instance!!! don't just blindly copy!
     const bounds = new window.google.maps.LatLngBounds(center);
+    
     /*map.fitBounds(bounds);*/
 
     setMap(map)
@@ -39,7 +40,7 @@ function Maps() {
     <GoogleMap
       mapContainerStyle={containerStyle}
       center={center}
-      zoom={25} // 
+      zoom={20} // 
       onLoad={onLoad}
       onUnmount={onUnmount}
     >
